210a7525a30fc5764b2865a5d89ed84456290e67
amirhaziemtxmr <amirhaziemtxmr@gmail.com>
2018-12-28 17:39:40 +0800

    Added a few new buttons, image processing seems to not be working but the buttons seem to work fine. Thresholding works nicely too. Will update tomorrow

--- test/src/tkinter-opencv.py
+++ test/src/tkinter-opencv.py
@@ -8,12 +8,22 @@ Base code for GUI and openCV display obtained from @source.
 I added additional GUI functionalities and will add image processing code
 and threshold adjustment code later.
 '''
+import numpy as np
+from imutils import contours
+from skimage import measure 
+import imutils
+import datetime
 import tkinter
 import cv2
 import PIL.Image, PIL.ImageTk
 import time
+from Tools.scripts.texi2html import increment
 
+#default values, can be included in GUI
 cam = 0
+d = 15 #cm
+de = 40 #cm
+f = 0.375 #factor by which the distance is multiplied
 
 class App():
     def __init__(self, window, window_title, video_source=cam):
@@ -32,11 +42,28 @@ class App():
         self.btn_snapshot = tkinter.Button(window, text = "Snapshot", width = 50, command = self.snapshot)
         self.btn_snapshot.pack(anchor=tkinter.CENTER, expand = True)
         
+        #initialise values
+        [self.thresh_px_lo, self.thresh_px_hi, self.thresh_rd_lo, self.thresh_rd_hi] = [20000, 1, 12, 1000]
+        self.darkbg = False
+        self.overlay = True
+        
         #sliders/spinboxes for controlling threshold
-        self.sliders_px_hi = tkinter.Spinbox(window, from_=0, to=50, command = self.set_thresh)
+        self.sliders_px_hi = tkinter.Spinbox(window, from_=0, to=10000, command = self.set_thresh, increment = 100.0)
         self.sliders_px_hi.pack(anchor = tkinter.E)
-        self.sliders_px_lo = tkinter.Spinbox(window, from_=0, to=50, command = self.set_thresh)
+        self.sliders_px_lo = tkinter.Spinbox(window, from_=0, to=100, command = self.set_thresh)
         self.sliders_px_lo.pack(anchor = tkinter.E)
+        self.sliders_rd_hi = tkinter.Spinbox(window, from_=0, to=10000, command = self.set_thresh, increment = 100.0)
+        self.sliders_rd_hi.pack(anchor = tkinter.E)
+        self.sliders_rd_lo = tkinter.Spinbox(window, from_=0, to=100, command = self.set_thresh)
+        self.sliders_rd_lo.pack(anchor = tkinter.E)
+        
+        #buttons for controlling overlay, darkbg
+        self.btn_darkbg = tkinter.Button(window, text = "Dark Background", width = 20, command = self.set_darkbg())
+        self.btn_darkbg.pack(side = 'left')
+        self.btn_overlay = tkinter.Button(window, text = "Overlay", width = 20, command = self.set_overlay())
+        self.btn_overlay.pack(side = 'left')
+#         self.btn_snapshot = tkinter.Button(window, text = "Snapshot", width = 50, command = self.snapshot)
+#         self.btn_snapshot.pack(anchor=tkinter.CENTER, expand = True)
         
         #after it is called once, the update will be automatically called after every delay ms
         self.delay = 15
@@ -44,11 +71,37 @@ class App():
         
         self.window.mainloop()
     
+    #boolean, will return true or false
+    def set_darkbg(self):
+        if self.darkbg == True:
+            self.darkbg = False
+        else:
+            self.darkbg = True
+            
+    def get_darkbg(self):
+        return self.darkbg
+    
+    def set_overlay(self):
+        if self.overlay == True:
+            self.overlay = False
+        else:
+            self.overlay = True
+            
+    def get_overlay(self):
+        return self.overlay
+    
     def set_thresh(self):
         self.thresh_px_hi = self.sliders_px_hi.get()
         print("current hi thresh value: ",self.thresh_px_hi)
         self.thresh_px_lo = self.sliders_px_lo.get()
-        print("current hi thresh value: ",self.thresh_px_lo)
+        print("current low thresh value: ",self.thresh_px_lo)
+        self.thresh_rd_hi = self.sliders_rd_hi.get()
+        print("current low thresh value: ",self.thresh_px_lo)
+        self.thresh_rd_lo = self.sliders_rd_lo.get()
+        print("current low thresh value: ",self.thresh_px_lo)
+        
+    def get_thresh(self):
+        return [int(self.thresh_px_lo), int(self.thresh_px_hi), int(self.thresh_rd_lo), int(self.thresh_rd_hi)]
         
     def snapshot(self):
         #get a frame from video source
@@ -62,6 +115,7 @@ class App():
         ret,frame = self.vid.get_frame()
         
         if ret:
+            frame = self.mould_detection(frame)
             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
             self.canvas.create_image(0,0, image = self.photo, anchor = tkinter.NW)
         
@@ -73,7 +127,90 @@ class App():
             cv2.destroyAllWindows()
             
         self.window.after(self.delay, self.update)
+    
+    def mould_detection(self,frame):
+        image = frame
+        self.feed = frame
+        [pxthresh_low, pxthresh_high, r_low, r_high] = self.get_thresh()
+        darkbg =  self.get_darkbg()
+        overlay = self.get_overlay()
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #     gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
+        if darkbg:
+            thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)[1]
+        else:
+            thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)[1]
         
+        labels = measure.label(thresh, neighbors=8, background=0)
+        blur = cv2.GaussianBlur(gray,(5,5),0)
+        ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        thresh = th3
+        mask = np.zeros(thresh.shape, dtype="uint8")
+        
+        #loop over unique components
+        for label in np.unique(labels):
+            #ignores background labels
+            if label == 0:
+                continue
+            #otherwise, construct label mask and count pixels
+            labelMask = np.zeros(thresh.shape, dtype = "uint8")
+            labelMask[labels == label] = 255
+            numPixels = cv2.countNonZero(labelMask)
+    
+            #if the number of pixels in the component is small enough
+            # then add it to our mask of small blobs
+            
+            if numPixels > pxthresh_low and numPixels < pxthresh_high:
+                mask = cv2.add(mask, labelMask)
+                
+        
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        try:
+            cnts = contours.sort_contours(cnts)[0]
+        except:
+            print("No holes detected!")
+        #loop over contours
+        for i, c in enumerate(cnts):
+            (x, y, w, h) = cv2.boundingRect(c)
+            ((cX, cY), radius) = cv2.minEnclosingCircle(c)
+            
+    #         if radius > r_high or radius < r_low:
+    #             cnts[i] = 0
+            if radius < r_high and radius > r_low:
+                if overlay:
+                    cv2.circle(image, (int(cX), int(cY)), int(radius), (0,0,255), 3)
+                    print("circle no. {}".format(i+1), "radius:", radius, "center:", cX, ",", cY)
+                    cv2.putText(image, "hole # {}".format(i+1), (x,y -15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0,0,255), 2)
+                    cv2.rectangle(image, (int(cX-(w/2)), int(cY-(h/2))), (int(cX+(w/2)), int(cY+(h/2))), (255,0,0), 2)
+            
+                if cX > 320:
+                    coordX = (cX - 320)/d
+                else:
+                    coordX = -(320 - cX)/d
+            
+                coordY = (460 - cY)/d
+                [coordX, coordY] = [round(coordX, 2), round(coordY, 2)]
+                #converting h,w to real sizes from pixel size
+                [h,w] = [round(h/d, 2), round(w/d, 2)]
+                area = round(h*w, 2)
+                if overlay:
+                    cv2.putText(image, "coords: ({},{})".format(coordX, coordY), (int(cX - radius),int(cY +radius) + 20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255), thickness = 1)
+                    cv2.putText(image, "height and width: ({},{})".format(h, w), (int(cX - radius),int(cY +radius) + 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255), thickness = 1)
+                    cv2.putText(image, "area: ({})".format(area), (int(cX - radius),int(cY +radius) + 40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255), thickness = 1)
+                    cv2.line(image, (0, 460), (640, 460), color = 255)
+                    cv2.line(image, (320, 0), (320, 480), color = 255)
+                    cv2.circle(image, (320, 460), 5, (255, 0, 0), 1)
+                    self.feed = image
+                else:
+                    cv2.putText(thresh, "coords: ({},{})".format(coordX, coordY), (int(cX - radius),int(cY +radius) + 20), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255), thickness = 1)
+                    cv2.line(thresh, (0, 460), (640, 460), color = 255)
+                    cv2.line(thresh, (320, 0), (320, 480), color = 255)
+                    cv2.circle(thresh, (320, 460), 5, (255, 0, 0), 1)
+                    self.feed = thresh
+    
+        
+        return self.feed
 
 
 class MyVideoCapture():
